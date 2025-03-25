2bacd7188de776e680f1b5b7469cc21e71553e48
Cladinoc18 <citylavivas@gmail.com>
2025-03-25 13:35:15 -0500

    docker

---
+++ back/src/app.module.ts
@@ -0,0 +1,37 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
+import { ProductModule } from './product/product.module';
+import { UserModule } from './user/user.module';
+import typeOrmConfig from "./config/typeOrm"
+import { SeedModule } from './seeds/seeds.module';
+import { AuthModule } from './auth/auth.module';
+import { OrderModule } from './order/order.module';
+import { OrderDetailsModule } from './order_details/order_details.module';
+import { CategoryModule } from './category/category.module';
+
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [typeOrmConfig]
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => configService.get("typeorm")
+    }),
+    ProductModule,
+    UserModule,
+    SeedModule,
+    AuthModule,
+    OrderModule,
+    OrderDetailsModule,
+    CategoryModule
+  ],
+  controllers: [],
+  providers: [],
+})
+
+export class AppModule { }
